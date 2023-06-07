86c7ccd33b195fb8a593cfd7a6dd5e846ea2c8de
ManishStark <mstark825@gmail.com>
2023-06-07 20:07:19 +0530

    Security Packages Added

--- startup/routes.js
+++ startup/routes.js
@@ -3,7 +3,30 @@ const userRoute = require("../routes/userRoute");
 const todoRoute = require("../routes/todoRoute");
 const error = require("../middleware/error");
 
+// For Security must include following middlewares
+const helmet = require("helmet"); //this include some secure headers
+const morgan = require("morgan"); // log all apis request in console
+const rateLimit = require("express-rate-limit"); // limit request through per ip
+const xssClean = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "To many requests from this IP, try again after 1 hour..",
+});
+
 module.exports = function (app) {
+  // Middleware for security
+  app.use(helmet());
+  if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev")); //logging all api request and status code in console
+  }
+  app.use("/", limiter);
+  app.use(xssClean());
+  app.use(mongoSanitize());
+
+  // Router for APIs
   app.use(express.json({ limit: "10kb" }));
   app.use("/api/user", userRoute);
   app.use("/api/todo", todoRoute);
